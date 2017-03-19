de599b7d90cff255cbe687e201df1d57acfc646d
Rab Horner <Rabbles@users.noreply.github.com>
2017-03-19 19:46:25 +0000

    Add files via upload

---
+++ VehicleInsuranceTests/BusinessRules/CalculationRules/CalculationTests.cs
@@ -0,0 +1,327 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VehicleInsurance.BusinessRules.CalculationRules;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleInsurance.Enum;
+using VehicleInsurance.Model;
+
+namespace VehicleInsuranceTests.BusinessRules.CalculationRules
+{
+    [TestClass()]
+    public class CalcualtionTests
+    {
+        private Policy _policy;
+        private Driver _driver;
+
+        [TestInitialize]
+        public void Initialise()
+        {
+            _policy = new Policy()
+            {
+                PolicyStartDate = new DateTime(),
+                DriversOnPolicy = new ObservableCollection<Driver>()
+            };
+
+            _driver = new Driver()
+            {
+                ClaimsAssociatedToDriver = new ObservableCollection<Claim>(),
+                DateOfBirth = new DateTime(),
+                Occupation = new Occupation()
+            };
+        }
+
+
+
+        [TestMethod()]
+        public void ImplementRuleTestBasePremiumIs500()
+        {
+            //Assemble
+            Policy policy = _policy;
+            double premium = 0;
+            BasePremiumRule rule = new BasePremiumRule();
+
+            //Act
+            double expectedResult = 500.00;
+            double actualResult = rule.ImplementRule(policy, premium);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        /// <summary>
+        /// Boundary test under 500.00
+        /// </summary>
+        [TestMethod()]
+        public void ImplementRuleTestBasePremiumIsLessThan500()
+        {
+            //Assemble
+
+            Policy policy = _policy;
+            double premium = 0;
+            BasePremiumRule rule = new BasePremiumRule();
+
+            //Act
+            double expectedResult = 499.00;
+            double actualResult = rule.ImplementRule(policy, premium);
+
+            //Assert
+            Assert.AreNotEqual(expectedResult, actualResult);
+        }
+
+        /// <summary>
+        /// Boundary test above 500.00
+        /// </summary>
+        [TestMethod()]
+        public void ImplementRuleTestBasePremiumIsMoreThan500()
+        {
+            //Assemble
+
+            Policy policy = _policy;
+            double premium = 0;
+            BasePremiumRule rule = new BasePremiumRule();
+
+            //Act
+            double expectedResult = 500.01;
+            double actualResult = rule.ImplementRule(policy, premium);
+
+            //Assert
+            Assert.AreNotEqual(expectedResult, actualResult);
+        }
+
+        /// <summary>
+        /// Other occupation test
+        /// </summary>
+        [TestMethod]
+        public void ImplementRuleTestOccupationOther()
+        {
+            //Assemble
+            Policy policy = _policy;
+            Driver driver = _driver;
+            driver.Occupation.JobTitle = OccupationEnum.Other;
+            policy.DriversOnPolicy.Add(driver);
+            double premium = 500;
+            DriverOccupationRule driverOccupationRule = new DriverOccupationRule();
+
+            //Act
+            double expectedResult = premium;
+            double actualResult = driverOccupationRule.ImplementRule(policy, premium);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        /// <summary>
+        /// Test for chauffeur.  Increase by 10%
+        /// </summary>
+        [TestMethod]
+        public void ImplementRuleTestOccupationChauffeur()
+        {
+            //Assemble
+            Policy policy = _policy;
+            Driver driver = _driver;
+            driver.Occupation.JobTitle = OccupationEnum.Chauffeur;
+            policy.DriversOnPolicy.Add(driver);
+            double premium = 500;
+            DriverOccupationRule driverOccupationRule = new DriverOccupationRule();
+
+            //Act
+            double expectedResult = premium + premium * 0.1;
+            double actualResult = driverOccupationRule.ImplementRule(policy, premium);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        /// <summary>
+        /// Test for Accountant.  Decrease by 10%
+        /// </summary>
+        [TestMethod]
+        public void ImplementRuleTestOccupationAccountant()
+        {
+            //Assemble
+            Policy policy = _policy;
+            Driver driver = _driver;
+            driver.Occupation.JobTitle = OccupationEnum.Accountant;
+            policy.DriversOnPolicy.Add(driver);
+            double premium = 500;
+            DriverOccupationRule driverOccupationRule = new DriverOccupationRule();
+
+            //Act
+            double expectedResult = premium - premium * 0.1;
+            double actualResult = driverOccupationRule.ImplementRule(policy, premium);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ImplementRuleYoungestDriver21()
+        {
+            //Assemble
+            Policy policy = _policy;
+            Driver driver = _driver;
+            DateTime startDate = new DateTime(2017, 03, 20);
+            DateTime driverDateOfBirth = new DateTime(1996, 03, 20);
+            policy.PolicyStartDate = startDate;
+            policy.DriversOnPolicy.Add(driver);
+            driver.DateOfBirth = driverDateOfBirth;
+            double premium = 500;
+            YoungestDriverRule youngestDriverRule = new YoungestDriverRule();
+
+            //Act
+
+            double expectedResult = premium + premium * 0.2;
+            double actualResult = youngestDriverRule.ImplementRule(policy, premium);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ImplementRuleYoungestDriver25()
+        {
+            //Assemble
+            Policy policy = _policy;
+            Driver driver = _driver;
+            DateTime startDate = new DateTime(2017, 03, 20);
+            DateTime driverDateOfBirth = new DateTime(1992, 03, 20);
+            policy.PolicyStartDate = startDate;
+            policy.DriversOnPolicy.Add(driver);
+            driver.DateOfBirth = driverDateOfBirth;
+            double premium = 500;
+            YoungestDriverRule youngestDriverRule = new YoungestDriverRule();
+
+            //Act
+
+            double expectedResult = premium + premium * 0.2;
+            double actualResult = youngestDriverRule.ImplementRule(policy, premium);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+        [TestMethod]
+        public void ImplementRuleYoungestDriver26()
+        {
+            //Assemble
+            Policy policy = _policy;
+            Driver driver = _driver;
+            DateTime startDate = new DateTime(2017, 03, 20);
+            DateTime driverDateOfBirth = new DateTime(1991, 03, 20);
+            policy.PolicyStartDate = startDate;
+            policy.DriversOnPolicy.Add(driver);
+            driver.DateOfBirth = driverDateOfBirth;
+            double premium = 500;
+            YoungestDriverRule youngestDriverRule = new YoungestDriverRule();
+
+            //Act
+
+            double expectedResult = premium + premium * 0.1;
+            double actualResult = youngestDriverRule.ImplementRule(policy, premium);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ImplementRuleYoungestDriver75()
+        {
+            //Assemble
+            Policy policy = _policy;
+            Driver driver = _driver;
+            DateTime startDate = new DateTime(2017, 03, 20);
+            DateTime driverDateOfBirth = new DateTime(1942, 03, 20);
+            policy.PolicyStartDate = startDate;
+            policy.DriversOnPolicy.Add(driver);
+            driver.DateOfBirth = driverDateOfBirth;
+            double premium = 500;
+            YoungestDriverRule youngestDriverRule = new YoungestDriverRule();
+
+            //Act
+
+            double expectedResult = premium + premium * 0.1;
+            double actualResult = youngestDriverRule.ImplementRule(policy, premium);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ImplementRuleClaimsWithinYear()
+        {
+            //Assemble
+            Policy policy = _policy;
+            Driver driver = _driver;
+            DateTime policyStartDate = new DateTime(2017, 03, 20);
+            policy.PolicyStartDate = policyStartDate;
+            DateTime claimDate = new DateTime(2016, 03, 20);
+            Claim claim = new Claim() { DateOfClaim = claimDate };
+            DriverClaimsRule driverClaimsRule = new DriverClaimsRule();
+            driver.ClaimsAssociatedToDriver.Add(claim);
+            policy.DriversOnPolicy.Add(driver);
+            double premium = 500;
+
+            //Act
+            double expectedResult = premium + premium * 0.2;
+            double actualResult = driverClaimsRule.ImplementRule(policy, premium);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+        [TestMethod]
+        public void ImplementRuleClaimsWithinTwoYears()
+        {
+            //Assemble
+            Policy policy = _policy;
+            Driver driver = _driver;
+            DateTime policyStartDate = new DateTime(2017, 03, 20);
+            policy.PolicyStartDate = policyStartDate;
+            DateTime claimDate = new DateTime(2015, 03, 20);
+            Claim claim = new Claim() { DateOfClaim = claimDate };
+            DriverClaimsRule driverClaimsRule = new DriverClaimsRule();
+            driver.ClaimsAssociatedToDriver.Add(claim);
+            policy.DriversOnPolicy.Add(driver);
+            double premium = 500;
+
+            //Act
+            double expectedResult = premium + premium * 0.1;
+            double actualResult = driverClaimsRule.ImplementRule(policy, premium);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+        [TestMethod]
+        public void ImplementRuleClaimsWithinFiveYears()
+        {
+            //Assemble
+            Policy policy = _policy;
+            Driver driver = _driver;
+            DateTime policyStartDate = new DateTime(2017, 03, 20);
+            policy.PolicyStartDate = policyStartDate;
+            DateTime claimDate = new DateTime(2012, 03, 20);
+            Claim claim = new Claim() { DateOfClaim = claimDate };
+            DriverClaimsRule driverClaimsRule = new DriverClaimsRule();
+            driver.ClaimsAssociatedToDriver.Add(claim);
+            policy.DriversOnPolicy.Add(driver);
+            double premium = 500;
+
+            //Act
+            double expectedResult = premium + premium * 0.1;
+            double actualResult = driverClaimsRule.ImplementRule(policy, premium);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ VehicleInsuranceTests/BusinessRules/DeclineRules/DeclineTests.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VehicleInsurance.BusinessRules.DeclinedRules;
+using VehicleInsurance.Model;
+
+namespace VehicleInsuranceTests.BusinessRules.DeclineRules
+{
+    [TestClass()]
+    public class DeclineTests
+    {
+        private Policy _policy;
+        private Driver _driver;
+        private Claim _claim;
+
+
+        [TestInitialize]
+        public void Initialise()
+        {
+            _policy = new Policy()
+            {
+                PolicyStartDate = new DateTime(),
+                DriversOnPolicy = new ObservableCollection<Driver>()
+            };
+
+            _driver = new Driver()
+            {
+                ClaimsAssociatedToDriver = new ObservableCollection<Claim>(),
+                DateOfBirth = new DateTime(),
+                Occupation = new Occupation()
+            };
+
+            _claim = new Claim()
+            {
+                DateOfClaim = new DateTime()
+            };
+        }
+
+        [TestMethod]
+        //check to test date before today fails
+        public void ImplementRuleTestBadStartDatePast()
+        {
+            //Assemble
+            Policy policy = _policy;
+            policy.PolicyStartDate = DateTime.Today.AddDays(-1);
+            BadStartDateRule badStartDate = new BadStartDateRule();
+
+            //Act
+            bool expectedResult = false;
+            bool actualResult = badStartDate.ImplementRule(policy).IsSuccessful;
+            
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        //check to test todays date passes
+        public void ImplementRuleTestBadStartDateNow()
+        {
+            //Assemble
+            Policy policy = _policy;
+            policy.PolicyStartDate = DateTime.Today;
+            BadStartDateRule badStartDate = new BadStartDateRule();
+
+            //Act
+            bool expectedResult = true;
+            bool actualResult = badStartDate.ImplementRule(policy).IsSuccessful;
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        //test if driver is 21 or over
+        public void ImplementRuleTestYoungestDriverIs21()
+        {
+            //Assemble
+            Policy policy = _policy;
+            Driver driver = _driver;
+            DateTime startDate = new DateTime(2017, 03, 20);
+            DateTime driverDateOfBirth = new DateTime(1996, 03, 20);
+            policy.PolicyStartDate = startDate;
+            policy.DriversOnPolicy.Add(driver);
+            driver.DateOfBirth = driverDateOfBirth;
+            YoungestDriverUnder21Rule driverUnder21 = new YoungestDriverUnder21Rule();
+
+            //Act
+            bool expectedResult = true;
+            bool actualResult = driverUnder21.ImplementRule(policy).IsSuccessful;
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ImplementRuleTestYoungestDriverIsUnder21()
+        {
+            //Assemble
+            Policy policy = _policy;
+            Driver driver = _driver;
+            DateTime startDate = new DateTime(2017, 03, 20);
+            DateTime driverDateOfBirth = new DateTime(1997, 03, 20);
+            policy.PolicyStartDate = startDate;
+            policy.DriversOnPolicy.Add(driver);
+            driver.DateOfBirth = driverDateOfBirth;
+            YoungestDriverUnder21Rule driverUnder21 = new YoungestDriverUnder21Rule();
+
+            //Act
+            bool expectedResult = false;
+            bool actualResult = driverUnder21.ImplementRule(policy).IsSuccessful;
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ImplementRuleTestOldestDriverIs75()
+        {
+            //Assemble
+            Policy policy = _policy;
+            Driver driver = _driver;
+            DateTime startDate = new DateTime(2017, 03, 20);
+            DateTime driverDateOfBirth = new DateTime(1942, 03, 20);
+            policy.PolicyStartDate = startDate;
+            policy.DriversOnPolicy.Add(driver);
+            driver.DateOfBirth = driverDateOfBirth;
+            OldestDriverOver75Rule over75Rule = new OldestDriverOver75Rule();
+
+            //Act
+            bool expectedResult = true;
+            bool actualResult = over75Rule.ImplementRule(policy).IsSuccessful;
+
+
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        [TestMethod]
+        //One year boundary check
+        public void ImplementRuleTestOldestDriverIsOver75()
+        {
+            //Assemble
+            Policy policy = _policy;
+            Driver driver = _driver;
+            DateTime startDate = new DateTime(2017, 03, 20);
+            DateTime driverDateOfBirth = new DateTime(1941, 03, 20);
+            policy.PolicyStartDate = startDate;
+            policy.DriversOnPolicy.Add(driver);
+            driver.DateOfBirth = driverDateOfBirth;
+            OldestDriverOver75Rule over75Rule = new OldestDriverOver75Rule();
+
+            //Act
+            bool expectedResult = false;
+            bool actualResult = over75Rule.ImplementRule(policy).IsSuccessful;
+
+            //Assert
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        [TestMethod]
+        public void ImplementRuleTestExceedTwoClaimsPerDriver()
+        {
+            //Assemble
+            Policy policy = _policy;
+            Driver driver = _driver;
+            Claim claim = _claim;
+            int maxClaims = 2;
+
+            //exceed allowable claims
+            for (int i = 0; i < maxClaims + 1; i++)
+            {
+                driver.ClaimsAssociatedToDriver.Add(claim);
+            }
+
+            policy.DriversOnPolicy.Add(driver);
+
+            DriverHasMoreThanTwoClaimsRule driverMoreThanTwoClaims = new DriverHasMoreThanTwoClaimsRule();
+           
+           
+            //Act
+            bool expectedResult = false;
+            bool actualResult = driverMoreThanTwoClaims.ImplementRule(policy).IsSuccessful;
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ImplementTestRuleTotalClaimsIsThree()
+        {
+            //Assemble
+            Policy policy = _policy;
+            Claim claim = new Claim();
+
+            //Only three drivers required
+            Driver driverA = new Driver()
+            {
+                DateOfBirth = new DateTime(),
+                Occupation = new Occupation(),
+                ClaimsAssociatedToDriver = new ObservableCollection<Claim>()
+            };
+
+            Driver driverB = new Driver()
+            {
+                DateOfBirth = new DateTime(),
+                Occupation = new Occupation(),
+                ClaimsAssociatedToDriver = new ObservableCollection<Claim>()
+            };
+
+            Driver driverC = new Driver()
+            {
+                DateOfBirth = new DateTime(),
+                Occupation = new Occupation(),
+                ClaimsAssociatedToDriver = new ObservableCollection<Claim>()
+            };
+
+            driverA.ClaimsAssociatedToDriver.Add(claim);
+            driverB.ClaimsAssociatedToDriver.Add(claim);
+            driverC.ClaimsAssociatedToDriver.Add(claim);
+
+            policy.DriversOnPolicy.Add(driverA);
+            policy.DriversOnPolicy.Add(driverB);
+            policy.DriversOnPolicy.Add(driverC);
+
+            TotalClaimsExceedsThreeRule exceedsThree = new TotalClaimsExceedsThreeRule();
+
+            //Act
+            bool expectedResult = true;
+            bool actualResult = exceedsThree.ImplementRule(policy).IsSuccessful;
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        //Same as above but add an additional claim to driverA
+        public void ImplementTestRuleTotalClaimsExceedsThree()
+        {
+            //Assemble
+            Policy policy = _policy;
+            Claim claim = new Claim();
+
+            //Only three drivers required
+            Driver driverA = new Driver()
+            {
+                DateOfBirth = new DateTime(),
+                Occupation = new Occupation(),
+                ClaimsAssociatedToDriver = new ObservableCollection<Claim>()
+            };
+
+            Driver driverB = new Driver()
+            {
+                DateOfBirth = new DateTime(),
+                Occupation = new Occupation(),
+                ClaimsAssociatedToDriver = new ObservableCollection<Claim>()
+            };
+
+            Driver driverC = new Driver()
+            {
+                DateOfBirth = new DateTime(),
+                Occupation = new Occupation(),
+                ClaimsAssociatedToDriver = new ObservableCollection<Claim>()
+            };
+
+            driverA.ClaimsAssociatedToDriver.Add(claim);
+            driverB.ClaimsAssociatedToDriver.Add(claim);
+            driverC.ClaimsAssociatedToDriver.Add(claim);
+
+            policy.DriversOnPolicy.Add(driverA);
+            policy.DriversOnPolicy.Add(driverB);
+            policy.DriversOnPolicy.Add(driverC);
+
+            //create excessive claim
+            driverA.ClaimsAssociatedToDriver.Add(claim);
+
+            TotalClaimsExceedsThreeRule exceedsThree = new TotalClaimsExceedsThreeRule();
+
+            //Act
+            bool expectedResult = false;
+            bool actualResult = exceedsThree.ImplementRule(policy).IsSuccessful;
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+    }
+    }
+
